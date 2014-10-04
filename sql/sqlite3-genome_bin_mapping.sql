8adebb77ab5e330b14ede87f63e1082898618342
ctSkennerton <c.skennerton@gmail.com>
2014-10-04 16:26:44 -0700

    Add in source files

---
+++ sql/sqlite3-genome_bin_mapping.sql
@@ -0,0 +1,25 @@
+-- The biosql data model treats a top level record as an individual
+-- sequence like a contig as a separate entity. But in the world of
+-- draft genomes we usually want to link these together as a genome
+-- in many pieces. This is a simple mapping of bioentries that are
+-- found in a BioSQL database to their genome
+CREATE TABLE genome_sequence(
+    genome_id             INTEGER,
+    bioentry_id           INTEGER,
+);
+
+-- Store some basic metadata about our genomes. For now just the name,
+-- description and the taxon id should be good enough. Also included
+-- is a temporary place to store a taxonomy string if the genome is
+-- currently not in genbank and therefore would not have a taxon id.
+-- Since the taxon info of BioSQL already stores the name and the tax
+-- string in various parts the name, and tmp_tax_string would only
+-- needed to be used for novel genomes with the taxon_id field left
+-- blank.
+CREATE TABLE genome(
+    genome_id             INTEGER PRIMARY KEY,
+    taxon_id              INTEGER,
+    name                  TEXT,
+    description           TEXT,
+    tmp_tax_string        TEXT
+);
---
+++ sql/sqlite3-gff_view.sql
@@ -0,0 +1,41 @@
+-- this isn't quite a perfect view as the source, score and phase
+-- attributes are left as dummy values. On gff3 files I've loaded
+-- these are all stored in the attributes of the record so it
+-- would be possible to get them out with a more complicated query
+-- but there columns are the least useful bits of information so
+-- for now I will leave them be
+CREATE VIEW gff
+AS
+    SELECT   gb.fref                                      AS seqid,
+             NULL                                         AS source,
+             gb.type                                      AS type,
+             gb.fstart                                    AS start,
+             gb.fend                                      AS end,
+             0                                            AS score,
+             gb.fstrand                                   AS strand,
+             0                                            AS phase,
+             group_concat(t.name || "=" || qv.value, ";") AS attributes
+    FROM     gff_base gb
+    JOIN     term t,
+             seqfeature_qualifier_value qv
+    ON       qv.term_id = t.term_id                       AND
+             qv.seqfeature_id = gb.gid
+    GROUP BY qv.seqfeature_id;
+
+-- basic gff information without the attributes text
+CREATE VIEW gff_base
+AS
+    SELECT   e.accession                                  AS fref,
+             fl.start_pos                                 AS fstart,
+             fl.end_pos                                   AS fend,
+             k.name                                       AS type,
+             fl.strand                                    AS fstrand,
+             f.seqfeature_id                              AS gid
+    FROM     seqfeature f,
+             term k,
+             location fl,
+             bioentry e
+    WHERE    f.type_term_id = k.term_id                   AND
+             fl.seqfeature_id = f.seqfeature_id           AND
+             f.bioentry_id    = e.bioentry_id;
+
