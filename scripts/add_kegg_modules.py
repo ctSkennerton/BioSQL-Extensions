e9fdfec0085b14d0c9d33c95ee06923e2f9d96cc
ctSkennerton <c.skennerton@gmail.com>
2016-01-19 16:33:40 -0800

    fixes to make adding kegg data work

--- scripts/add_kegg_modules.py
+++ scripts/add_kegg_modules.py
@@ -13,10 +13,10 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--database', help='name of premade biosql database')
     parser.add_argument('-D', '--database-name', help='namespace of the database that you want to add into', dest='database_name', default='metagenomic_database')
     parser.add_argument('-r', '--driver', help='Python database driver to use (must be installed separately)', choices=["MySQLdb", "psycopg2", "sqlite3"], default='psycopg2')
+    parser.add_argument('-H', '--host', help='host to connect to', default='localhost')
     parser.add_argument('-p', '--port', help='post to connect to on the host')
     parser.add_argument('-u', '--user', help='database user name')
     parser.add_argument('-P','--password', help='database password for user')
-    parser.add_argument('-H', '--host', help='host to connect to', default='localhost')
     parser.add_argument('-o', '--ontology', help='give a name for this ontology', default='KEGG_modules')
 
     args = parser.parse_args()
@@ -27,15 +27,15 @@ if __name__ == '__main__':
     rows = server.adaptor.execute_and_fetchall('select ontology_id from ontology where name = %s', (args.ontology,))
     if len(rows) == 0:
         server.adaptor.execute('insert into ontology (name, definition) values(%s, %s)', (args.ontology, 'The Kegg module ontology'))
-        kegg_ontology = server.adaptor.execute_and_fetch_col0('select ontology_id from ontology where name = %s', (args.ontology))[0]
+        kegg_ontology = server.adaptor.execute_and_fetch_col0('select ontology_id from ontology where name = %s', (args.ontology, ))[0]
     else:
         kegg_ontology = rows[0][0]
 
     # Get the predicate term id
-    rows = server.adaptor.execute_and_fetchall('select term_id from term where name = "partOf"')
+    rows = server.adaptor.execute_and_fetchall('select term_id from term where name = \'partOf\'')
     if len(rows) == 0:
         server.adaptor.execute('insert into term (name, definition) values(%s, %s)', ('partOf', 'Used as a predicate when defining the relationship between terms'))
-        predicate_id = server.adaptor.execute_and_fetch_col0('select term_id from term where name = "partOf"')[0]
+        predicate_id = server.adaptor.execute_and_fetch_col0('select term_id from term where name = \'partOf\'')[0]
     else:
         predicate_id = rows[0][0]
 
@@ -65,7 +65,7 @@ if __name__ == '__main__':
 
                     print '            -',l4,d4['definition']
                     server.adaptor.execute('insert into term (identifier, name, ontology_id) values (%s, %s, %s)', (l4, d4['definition'], kegg_ontology))
-                    l4_id = server.adaptor.execute_and_fetch_col0('select term_id from term where name = %s and ontology_id = %s', (l4, kegg_ontology))[0]
+                    l4_id = server.adaptor.execute_and_fetch_col0('select term_id from term where identifier = %s and ontology_id = %s', (l4, kegg_ontology))[0]
                     server.adaptor.execute('insert into term_relationship (subject_term_id, predicate_term_id, object_term_id, ontology_id) values (%s, %s, %s, %s)', (l3_id, predicate_id, l4_id, kegg_ontology))
                     server.adaptor.execute('insert into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l1_id, predicate_id, l4_id, kegg_ontology, 3))
                     server.adaptor.execute('insert into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l2_id, predicate_id, l4_id, kegg_ontology, 2))
@@ -82,9 +82,9 @@ if __name__ == '__main__':
                             ortho_id = server.adaptor.execute_and_fetch_col0('select term_id from term where identifier = %s and name = %s and ontology_id = %s', (ortholog, ortholog, kegg_ontology) )[0]
 
                         server.adaptor.execute('insert into term_relationship (subject_term_id, predicate_term_id, object_term_id, ontology_id) values (%s, %s, %s, %s)', (l4_id, predicate_id, ortho_id, kegg_ontology))
-                        server.adaptor.execute('insert or ignore into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l1_id, predicate_id, ortho_id, kegg_ontology, 4))
-                        server.adaptor.execute('insert or ignore into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l2_id, predicate_id, ortho_id, kegg_ontology, 3))
-                        server.adaptor.execute('insert or ignore into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l3_id, predicate_id, ortho_id, kegg_ontology, 2))
+                        server.adaptor.execute('insert into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l1_id, predicate_id, ortho_id, kegg_ontology, 4))
+                        server.adaptor.execute('insert into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l2_id, predicate_id, ortho_id, kegg_ontology, 3))
+                        server.adaptor.execute('insert into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l3_id, predicate_id, ortho_id, kegg_ontology, 2))
                         server.adaptor.execute('insert into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l4_id, predicate_id, ortho_id, kegg_ontology, 1))
 
     server.commit()
--- scripts/add_kegg_orthology.py
+++ scripts/add_kegg_orthology.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--database', help='name of premade biosql database')
     parser.add_argument('-D', '--database-name', help='namespace of the database that you want to add into', dest='database_name', default='metagenomic_database')
     parser.add_argument('-r', '--driver', help='Python database driver to use (must be installed separately)', choices=["MySQLdb", "psycopg2", "sqlite3"], default='psycopg2')
+    parser.add_argument('-H', '--host', help='host to connect to', default='localhost')
     parser.add_argument('-p', '--port', help='post to connect to on the host')
     parser.add_argument('-u', '--user', help='database user name')
     parser.add_argument('-P','--password', help='database password for user')
@@ -23,7 +24,7 @@ if __name__ == '__main__':
 
 
     # create an ontology for KEGG if it doesn't already exist
-    rows = server.adaptor.execute_and_fetchall('select ontology_id from ontology where name = %s', (args.ontology))
+    rows = server.adaptor.execute_and_fetchall('select ontology_id from ontology where name = %s', (args.ontology,))
     if len(rows) == 0:
         server.adaptor.execute('insert into ontology (name, definition) values(%s, %s)', (args.ontology, 'The Kegg ortholog ontology'))
         kegg_ontology = server.adaptor.execute_and_fetch_col0('select ontology_id from ontology where name = %s', (args.ontology))[0]
@@ -31,10 +32,10 @@ if __name__ == '__main__':
         kegg_ontology = rows[0][0]
 
     # Get the predicate term id
-    rows = server.adaptor.execute_and_fetchall('select term_id from term where name = "partOf"')
+    rows = server.adaptor.execute_and_fetchall('select term_id from term where name = \'partOf\'')
     if len(rows) == 0:
-        server.adaptor.execute('insert into term (name, definition) values(%s, %s)', ('partOf', 'Used as a predicate when defining the relationship between terms'))
-        predicate_id = server.adaptor.execute_and_fetch_col0('select term_id from term where name = "partOf"')[0]
+        server.adaptor.execute('insert into term (name, definition, ontology_id) values(%s, %s, %s)', ('partOf', 'Used as a predicate when defining the relationship between terms', kegg_ontology))
+        predicate_id = server.adaptor.execute_and_fetch_col0('select term_id from term where name = \'partOf\'')[0]
     else:
         predicate_id = rows[0][0]
 
@@ -57,7 +58,9 @@ if __name__ == '__main__':
 
                 print '        -',l3,d3['definition']
                 server.adaptor.execute('insert into term (identifier, name, ontology_id) values (%s, %s, %s)', (l3, d3['definition'], kegg_ontology))
-                l3_id = server.adaptor.execute_and_fetch_col0('select term_id from term where name = %s and ontology_id = %s', (l3, kegg_ontology))[0]
+
+                l3_id = server.adaptor.execute_and_fetch_col0('select term_id from term where identifier = %s and ontology_id = %s', (l3, kegg_ontology))[0]
+
                 server.adaptor.execute('insert into term_relationship (subject_term_id, predicate_term_id, object_term_id, ontology_id) values (%s, %s, %s, %s)', (l2_id, predicate_id, l3_id, kegg_ontology))
                 server.adaptor.execute('insert into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l1_id, predicate_id, l3_id, kegg_ontology, 2))
                 server.adaptor.execute('insert into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l2_id, predicate_id, l3_id, kegg_ontology, 1))
@@ -71,10 +74,10 @@ if __name__ == '__main__':
                         server.adaptor.execute('insert into term (identifier, name, ontology_id) values(%s,%s,%s)', (ortholog, ortholog, kegg_ontology))
                         ortho_id = server.adaptor.execute_and_fetch_col0('select term_id from term where identifier = %s and name = %s and ontology_id = %s', (ortholog, ortholog, kegg_ontology) )[0]
 
-                    server.adaptor.execute('insert or ignore into term_relationship (subject_term_id, predicate_term_id, object_term_id, ontology_id) values (%s, %s, %s, %s)', (l3_id, predicate_id, ortho_id, kegg_ontology))
-                    server.adaptor.execute('insert or ignore into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l1_id, predicate_id, ortho_id, kegg_ontology, 3))
-                    server.adaptor.execute('insert or ignore into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l2_id, predicate_id, ortho_id, kegg_ontology, 2))
-                    server.adaptor.execute('insert or ignore into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l3_id, predicate_id, ortho_id, kegg_ontology, 1))
+                    server.adaptor.execute('insert into term_relationship (subject_term_id, predicate_term_id, object_term_id, ontology_id) values (%s, %s, %s, %s)', (l3_id, predicate_id, ortho_id, kegg_ontology))
+                    server.adaptor.execute('insert into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l1_id, predicate_id, ortho_id, kegg_ontology, 3))
+                    server.adaptor.execute('insert into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l2_id, predicate_id, ortho_id, kegg_ontology, 2))
+                    server.adaptor.execute('insert into term_path (subject_term_id, predicate_term_id, object_term_id, ontology_id, distance) values (%s,%s,%s,%s,%s)', (l3_id, predicate_id, ortho_id, kegg_ontology, 1))
 
     server.commit()
 
