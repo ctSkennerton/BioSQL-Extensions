36c3f4905ef86a89c32b458e057456265efabe73
ctSkennerton <c.skennerton@gmail.com>
2016-02-09 16:23:05 -0800

    add in new scripts

---
+++ scripts/extract_sequences_from_taxonomy.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+import argparse
+import sqlite3
+from BioSQL import BioSeqDatabase
+from BioSQL.BioSeq import DBSeqRecord
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio import Entrez
+Entrez.email = 'c.skennerton@gmail.com'
+
+class FileManager(object):
+    def __init__(self, taxid, filename):
+        self.t = taxid
+        self.f = filename
+        self.a = False
+
+
+def extract_feature(dbrec, output_format, fp):
+
+    for feature in dbrec.features:
+        feat_extract = feature.extract(dbrec.seq.toseq())
+        if output_format == 'feat-prot':
+            feat_extract = feat_extract.translate()
+        try:
+            seqid = feature.qualifiers['ID'][0]
+        except KeyError:
+            try:
+                seqid = feature.qualifiers['locus_tag'][0]
+            except KeyError:
+                seqid = feature._seqfeature_id
+
+        description = ''
+        try:
+            description += ' '.join(feature.qualifiers['product'])
+        except KeyError:
+            pass
+        else:
+            description += ' '
+
+        try:
+            description += ' '.join(feature.qualifiers['gene'])
+        except KeyError:
+            pass
+
+        feat_extract = SeqRecord(feat_extract, id=seqid, description=description)
+        SeqIO.write(feat_extract, fp, 'fasta')
+
+def main(args):
+    server = BioSeqDatabase.open_database(driver=args.driver, db=args.database, user=args.user, host=args.host, passwd=args.password)
+
+    try:
+        ncbi_tax = int(args.taxid)
+        taxon_id_lookup_sql = "SELECT bioentry_id, taxon_id FROM bioentry WHERE taxon_id IN "\
+                "(SELECT DISTINCT include.taxon_id FROM taxon "\
+                "INNER JOIN taxon as include ON (include.left_value "\
+                "BETWEEN taxon.left_value AND taxon.right_value) "\
+                "WHERE taxon.taxon_id  = %s) AND include.right_value = include.left_value + 1)"
+
+        rows = server.adaptor.execute_and_fetchall(taxon_id_lookup_sql, (ncbi_tax,))
+    except:
+        taxon_name_lookup_sql = "SELECT bioentry_id, taxon_id FROM bioentry WHERE taxon_id IN "\
+                "(SELECT DISTINCT include.taxon_id FROM taxon "\
+                "INNER JOIN taxon as include ON (include.left_value "\
+                "BETWEEN taxon.left_value AND taxon.right_value) "\
+                "WHERE taxon.taxon_id IN (SELECT taxon_id FROM taxon_name "\
+                "WHERE name like %s) AND include.right_value = include.left_value + 1)"
+
+        rows = server.adaptor.execute_and_fetchall(taxon_name_lookup_sql, (args.taxid,))
+    finally:
+        dbids = dict(rows)
+        files = {}
+        taxid_to_dbids = {}
+        if args.split_species:
+            taxon_file_mapping = {}
+            for k, v in dbids.items():
+                tname = server.adaptor.execute_and_fetch_col0("SELECT name from taxon_name where taxon_id = %s", (v,))[0]
+                tname = tname.replace(' ', '_')
+                if args.output_format == 'gb':
+                    tname += '.gb'
+                elif args.output_format == 'feat-prot':
+                    tname += '.faa'
+                else:
+                    tname += '.fna'
+                files[v] = tname
+                taxid_to_dbids.setdefault(v, []).append(k)
+
+
+    if args.split_species:
+        # got to save all of the records before printing them out
+        outdata = {}
+        for db in server.values():
+            for taxid, dbid_list in taxid_to_dbids.items():
+                for dbid in dbid_list:
+                    try:
+                        seq_rec = db[dbid]
+                        outdata.setdefault(taxid, []).append(seq_rec)
+                    except KeyError:
+                        pass
+
+        for taxid, dbrecs in outdata.items():
+            with open(files[taxid], 'w') as fp:
+                if 'feat' in args.output_format:
+                    for dbrec in dbrecs:
+                        extract_feature(dbrec, args.output_format, fp)
+                else:
+                    SeqIO.write(dbid_s, fp, args.output_format)
+
+    else:
+        for dbname in server:
+            db = server[dbname]
+            for dbid, taxid in dbids.items():
+                try:
+                    dbrec = db[dbid]
+                    if 'feat' in args.output_format:
+                        extract_feature(dbrec, args.output_format, sys.stdout)
+                    else:
+                        SeqIO.write(dbrec, sys.stdout, args.output_format)
+                except KeyError:
+                    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--database', help='name of premade biosql database')
+    parser.add_argument('-D', '--database-name', help='namespace of the database that you want to add into', dest='database_name', default='metagenomic_database')
+    parser.add_argument('-r', '--driver', help='Python database driver to use (must be installed separately)', choices=["MySQLdb", "psycopg2", "sqlite3"], default='psycopg2')
+    parser.add_argument('-p', '--port', help='post to connect to on the host')
+    parser.add_argument('-u', '--user', help='database user name')
+    parser.add_argument('-P', '--password', help='database password for user')
+    parser.add_argument('-H', '--host', help='host to connect to', default='localhost')
+    parser.add_argument('-o', '--output_format', help='output format of the selected sequences', choices=['fasta', 'gb', 'feat-prot', 'feat-nucl'], default='fasta')
+    parser.add_argument('taxid', help='supply a ncbi taxonomy id that will be extracted. If an integer is supplied it will be interpreted as an NCBI taxonomy id; otherwise it will be interpreted as part of a taxonomy name (e.g. Proteobacteria)', default=None)
+    parser.add_argument('-s', '--split_species', help='when there are multiple species to be returned, split them into separate files, based on their name, instead of printing to stdout', default=False, action='store_true')
+    args = parser.parse_args()
+    main(args)
+
---
+++ scripts/modify_taxonomy_for_sequences.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+import sys
+import argparse
+import sqlite3
+from BioSQL import BioSeqDatabase
+from BioSQL import Loader
+from Bio import SeqIO
+from Bio import Entrez
+Entrez.email = 'c.skennerton@gmail.com'
+
+def add_taxid(inIter, taxid):
+    inIter.annotations['ncbi_taxid'] = taxid
+    return inIter
+
+def update_left_right_taxon_values(server, left_value):
+    """ update the left and right values in the table
+    """
+    if not left_value:
+        return
+    # Due to the UNIQUE constraint on the left and right values in the taxon
+    # table we cannot simply update them through an SQL statement as we risk
+    # colliding values. Instead we must select all of the rows that we want to
+    # update, modify the values in python and then update the rows
+    #self.adaptor.execute("UPDATE taxon SET right_value = right_value + 2 WHERE right_value >= %s", (left_value,))
+    #self.adaptor.execute("UPDATE taxon SET left_value = left_value + 2 WHERE left_value > %s", (left_value,))
+
+    rows = server.adaptor.execute_and_fetchall(
+            "SELECT left_value, right_value, taxon_id FROM taxon WHERE right_value >= %s or left_value > %s",
+            #"SELECT left_value, right_value, taxon_id FROM taxon",
+            (left_value, left_value)
+            )
+
+    right_rows = []
+    left_rows = []
+    for i in range(len(rows)):
+        new_right = rows[i][1]
+        new_left = rows[i][0]
+        if new_right >= left_value:
+            new_right += 2
+
+        if new_left > left_value:
+            new_left += 2
+        right_rows.append((new_right, rows[i][2]))
+        left_rows.append((new_left, rows[i][2]))
+
+
+    # sort the rows based on the value from largest to smallest
+    # should ensure no overlaps
+    right_rows = sorted(right_rows, key=lambda x: x[0], reverse=True)
+    left_rows = sorted(left_rows, key=lambda x: x[0], reverse=True)
+
+    try:
+        server.adaptor.executemany("UPDATE taxon SET left_value = %s WHERE taxon_id = %s", left_rows)
+        server.adaptor.executemany("UPDATE taxon SET right_value = %s WHERE taxon_id = %s", right_rows)
+    except:
+        #print(orig_rows)
+        #print(rows2)
+        #print(rows)
+
+        #for r in self.adaptor.execute_and_fetchall("select * from taxon"):
+        #    print(r)
+
+        raise
+
+def insert_taxon_rank(server, parent_taxon_id, parent_left_value, parent_right_value, node_name, node_rank):
+    # make sure that the name doesn't already exist
+    rows = server.adaptor.execute_and_fetch_col0('select taxon_id from taxon_name where name = %s', (node_name,))
+    if rows:
+        return server.adaptor.execute_and_fetchall(
+                'select taxon_id, left_value, right_value from taxon where taxon_id = %s ',
+                (rows[0], ))[0]
+    else:
+        left_value = parent_right_value
+        right_value = parent_right_value + 1
+        update_left_right_taxon_values(server, left_value)
+        server.adaptor.execute(
+                'insert into taxon (parent_taxon_id, node_rank, left_value, right_value)'
+                ' values (%s, %s, %s, %s)',
+                (parent_taxon_id, node_rank, left_value, right_value))
+
+        taxon_id = server.adaptor.execute_and_fetch_col0(
+                'select taxon_id from taxon where parent_taxon_id = %s '
+                'and left_value = %s and right_value = %s',
+                (parent_taxon_id, left_value, right_value))[0]
+
+        server.adaptor.execute(
+            "INSERT INTO taxon_name(taxon_id, name, name_class)"
+            " VALUES (%s, %s, 'scientific name')", (taxon_id,
+                                                    node_name[:255]))
+
+        return taxon_id, left_value, right_value
+
+def add_new_taxonomy(server, new_taxons, parent_ncbi_tax_id):
+    new_taxons = map(lambda x: x.split(":"), new_taxons)
+    parent_taxid = None
+    parent_left_value = None
+    parent_right_value = None
+    if parent_ncbi_tax_id:
+        parent_taxid, parent_left_value, parent_right_value = \
+                server.adaptor.execute_one(
+                        'select taxon_id, left_value, right_value '
+                        'from taxon where ncbi_taxon_id = %s',
+                        (parent_ncbi_tax_id,))
+        if not parent_taxid:
+            # the given ncbi taxonomy id isn't currently in the database
+            # download it using the Entrez API
+            db_loader = Loader.DatabaseLoader(server.adaptor, server.values()[0], True)
+            handle = Entrez.efetch( db="taxonomy", id=str(parent_ncbi_tax_id), retmode="XML")
+            taxon_record = Entrez.read(handle)
+            parent_taxid, parent_left_value, parent_right_value = db_loader._get_taxon_id_from_ncbi_lineage(
+                                        taxon_record[0]["LineageEx"])
+
+    for tax_name, tax_rank in new_taxons:
+        parent_taxid, parent_left_value, parent_right_value = insert_taxon_rank(server,
+                parent_taxid,
+                parent_left_value,
+                parent_right_value,
+                tax_name,
+                tax_rank)
+
+    return parent_taxid
+
+def main(args):
+    server = BioSeqDatabase.open_database(driver=args.driver, db=args.database, user=args.user, host=args.host, passwd=args.password)
+    if args.database_name not in server.keys():
+        server.new_database(args.database_name)
+
+    db = server[args.database_name]
+
+
+    taxon_id = add_new_taxonomy(server, args.new_taxons, args.taxid)
+
+    if args.fasta is not None:
+        gen = SeqIO.parse(args.fasta, 'fasta')
+    elif args.genbank is not None:
+        gen = SeqIO.parse(args.genbank, 'genbank')
+
+    for rec in gen:
+        server.adaptor.execute('update bioentry set taxon_id = %s where bioentry_id = %s',(taxon_id, db.adaptor.fetch_seqid_by_display_id(db.dbid, rec.name)))
+    server.commit()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--database', help='name of premade biosql database')
+    parser.add_argument('-D', '--database-name', help='namespace of the database that you want to add into', dest='database_name', default='metagenomic_database')
+    parser.add_argument('-r', '--driver', help='Python database driver to use (must be installed separately)', choices=["MySQLdb", "psycopg2", "sqlite3"], default='psycopg2')
+    parser.add_argument('-p', '--port', help='post to connect to on the host')
+    parser.add_argument('-u', '--user', help='database user name')
+    parser.add_argument('-P','--password', help='database password for user')
+    parser.add_argument('-H', '--host', help='host to connect to', default='localhost')
+    parser.add_argument('-f', '--fasta', help='fasta file to add into the database')
+    parser.add_argument('-G', '--genbank', help='genbank file to add into the database')
+    #parser.add_argument('-t', '--lookup-taxonomy', dest='tax_lookup', help='access taxonomy information on NCBI servers', action="store_true", default=False)
+    parser.add_argument('-T', '--taxid', help='supply a ncbi taxonomy id that will be applied to all sequences in the file, or if new_taxons are supplied on the command line this taxonomy ID will be used as the parent taxonomy for the novel lineages', required=True, default=None)
+    parser.add_argument('new_taxons', nargs="*", help='specify novel taxonomies not currenly in the NCBI database. each taxon specified on the command line should take the form of <taxon_name>:<taxon_rank>. Check the taxon table in the database for the appropriate values for the taxon_rank. e.g. ANME-2ab:family ANME-2b:genus ANME-hr1:species')
+    args = parser.parse_args()
+    main(args)
+
