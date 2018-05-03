f27eec8f3fbbb04130e11c0829b1e00d1f0d0260
ctSkennerton <c.skennerton@gmail.com>
2018-05-03 12:45:50 -0700

    don't have to add in the lineage

--- scripts/get_annotations_for_seqfeature.py
+++ scripts/get_annotations_for_seqfeature.py
@@ -41,17 +41,25 @@ def qv_dict(server, seqfeature_ids):
     qv = {}
     for feat_chunk in chunks(seqfeature_ids, 900):
         feat_chunk2 = tuple(feat_chunk)
-        qual_select_sql = 'SELECT seqfeature_id, name, value FROM seqfeature_qualifier_value qv, term t WHERE seqfeature_id IN ({}) AND t.term_id = qv.term_id'.format(generate_placeholders(len(feat_chunk)))
+        qual_select_sql = 'SELECT seqfeature_id, name, value FROM seqfeature_qualifier_value qv JOIN term t ON t.term_id = qv.term_id WHERE seqfeature_id IN ({})'.format(generate_placeholders(len(feat_chunk)))
 
-        taxonomy_sql = 'SELECT seqfeature_id, lineage.lineage FROM seqfeature JOIN bioentry USING(bioentry_id) JOIN lineage ON taxon_id = lineage.id WHERE seqfeature_id IN ({})'.format(generate_placeholders(len(feat_chunk)))
-        for seqfeature_id, lineage in server.adaptor.execute_and_fetchall(taxonomy_sql, feat_chunk2):
+        taxonomy_sql = 'SELECT seqfeature_id, bioentry.name, biodatabase.name, lineage.lineage FROM seqfeature JOIN bioentry USING(bioentry_id) JOIN biodatabase USING(biodatabase_id) LEFT JOIN lineage ON taxon_id = lineage.id WHERE seqfeature_id IN ({})'.format(generate_placeholders(len(feat_chunk)))
+        for seqfeature_id, contig, namespace, lineage in server.adaptor.execute_and_fetchall(taxonomy_sql, feat_chunk2):
             try:
-                qv[seqfeature_id]['taxonomy'] = lineage
-                qv[seqfeature_id]['organism'] = lineage.split(';')[-1]
+                if lineage:
+                    qv[seqfeature_id]['taxonomy'] = lineage
+                    qv[seqfeature_id]['organism'] = lineage.split(';')[-1]
+
+                qv[seqfeature_id]['bioentry'] = contig
+                qv[seqfeature_id]['sample'] = namespace
             except KeyError:
                 qv[seqfeature_id] = {}
-                qv[seqfeature_id]['taxonomy'] = lineage
-                qv[seqfeature_id]['organism'] = lineage.split(';')[-1]
+                if lineage:
+                    qv[seqfeature_id]['taxonomy'] = lineage
+                    qv[seqfeature_id]['organism'] = lineage.split(';')[-1]
+
+                qv[seqfeature_id]['bioentry'] = contig
+                qv[seqfeature_id]['sample'] = namespace
 
         for seqfeature_id, name, value in server.adaptor.execute_and_fetchall(qual_select_sql, feat_chunk2):
             if not name:
@@ -64,7 +72,7 @@ def qv_dict(server, seqfeature_ids):
 
     return qv
 
-def print_feature_qv_csv(server, sfids):
+def print_feature_qv_csv(server, sfids, outfile=sys.stdout):
     """raw sql extraction of fasta seqfeatures
     """
     qv = qv_dict(server, sfids)
@@ -78,7 +86,7 @@ def print_feature_qv_csv(server, sfids):
         columns |= set(data.keys())
 
     columns = sorted(columns)
-    writer = csv.writer(sys.stdout)
+    writer = csv.writer(outfile)
     writer.writerow(['seqfeature'] + columns)
     for sf, data in qv.items():
         row = [sf]
