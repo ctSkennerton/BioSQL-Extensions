6f032814e5735a95b4bd7c6fd4871935cb023757
ctSkennerton <c.skennerton@gmail.com>
2018-06-29 23:03:08 -0700

    add in more tests for export sequence

--- tests/test_biosqlx.py
+++ tests/test_biosqlx.py
@@ -8,6 +8,8 @@ import unittest
 from io import StringIO
 from click.testing import CliRunner
 
+from Bio import SeqIO
+
 from biosqlx import biosqlx
 from biosqlx import cli
 from . import connection_parameters
@@ -62,3 +64,228 @@ class TestExportSequence(unittest.TestCase):
         f.close()
         self.assertEqual(True, ',' in line)
 
+    def test_export_sequence_qv_feat_prot(self):
+        """Export from qualifier and value as translated proteins."""
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + ['-q', 'gene', '-v', 'dnaA', '-o', 'feat-prot'])
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'fasta'):
+            count += 1
+
+        f.close()
+        self.assertEqual(3, count)
+
+    def test_export_sequence_qv_feat_nucl(self):
+        """Export from qualifier and value as untranslated proteins."""
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + ['-q', 'gene', '-v', 'dnaA', '-o', 'feat-nucl'])
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'fasta'):
+            count += 1
+
+        f.close()
+        self.assertEqual(3, count)
+
+    def test_export_sequence_qv_fasta(self):
+        """Export from qualifier and value as fasta."""
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + ['-q', 'gene', '-v', 'dnaA', '-o', 'fasta'])
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'fasta'):
+            self.assertEqual(True, rec.id in ['NC_004347.2', 'NC_002939.5', 'NC_000913.3'])
+            count += 1
+
+        f.close()
+        self.assertEqual(3, count)
+
+    def test_export_sequence_qv_genbank(self):
+        """Export from qualifier and value as fasta."""
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + ['-q', 'gene', '-v', 'dnaA', '-o', 'gb'])
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'genbank'):
+            self.assertEqual(True, rec.id in ['NC_004347.2', 'NC_002939.5', 'NC_000913.3'])
+            count += 1
+
+        f.close()
+        self.assertEqual(3, count)
+
+    def test_export_sequence_qv_taxonomy_feat_prot(self):
+        """Export from taxonomy, qualifier and value as translated proteins."""
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + ['-t', 'Geobacter', '-q', 'gene', '-v', 'dnaA', '-o', 'feat-prot'])
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'fasta'):
+            count += 1
+
+        f.close()
+        self.assertEqual(1, count)
+
+    def test_export_sequence_qv_taxonomy_feat_nucl(self):
+        """Export from taxonomy, qualifier and value as untranslated proteins."""
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + ['-t', 'Geobacter', '-q', 'gene', '-v', 'dnaA', '-o', 'feat-nucl'])
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'fasta'):
+            count += 1
+
+        f.close()
+        self.assertEqual(1, count)
+
+    def test_export_sequence_qv_taxonomy_fasta(self):
+        """Export from taxonomy, qalifier and value as fasta."""
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + ['-t', 'Geobacter', '-q', 'gene', '-v', 'dnaA', '-o', 'fasta'])
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'fasta'):
+            self.assertEqual(rec.id, 'NC_002939.5')
+            count += 1
+
+        f.close()
+        self.assertEqual(1, count)
+
+    def test_export_sequence_qv_taxonomy_genbank(self):
+        """Export from taxonomy, qualifier and value as genbank."""
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + ['-t', 'Geobacter', '-q', 'gene', '-v', 'dnaA', '-o', 'gb'])
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'genbank'):
+            self.assertEqual(rec.id, 'NC_002939.5')
+            count += 1
+
+        f.close()
+        self.assertEqual(1, count)
+
+    def test_export_sequence_qv_namespace_feat_prot(self):
+        """Export from namespace, qualifier and value as translated proteins."""
+        fixture_params = ['-D', 'gammas', '-q', 'gene', '-v', 'dnaA', '-o', 'feat-prot']
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + fixture_params)
+
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'fasta'):
+            count += 1
+
+        f.close()
+        self.assertEqual(2, count)
+
+    def test_export_sequence_qv_namespace_feat_nucl(self):
+        """Export from namespace, qualifier and value as untranslated proteins."""
+        fixture_params = ['-D', 'gammas', '-q', 'gene', '-v', 'dnaA', '-o', 'feat-nucl']
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + fixture_params)
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'fasta'):
+            count += 1
+
+        f.close()
+        self.assertEqual(2, count)
+
+    def test_export_sequence_qv_namespace_fasta(self):
+        """Export from namespace, qalifier and value as fasta."""
+        fixture_params = ['-D', 'gammas', '-q', 'gene', '-v', 'dnaA', '-o', 'fasta']
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + fixture_params)
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'fasta'):
+            self.assertEqual(True, rec.id in ['NC_004347.2', 'NC_000913.3'])
+            count += 1
+
+        f.close()
+        self.assertEqual(2, count)
+
+    def test_export_sequence_qv_namespace_genbank(self):
+        """Export from taxonomy, qualifier and value as genbank."""
+        fixture_params = ['-D', 'gammas', '-q', 'gene', '-v', 'dnaA', '-o', 'gb']
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + fixture_params)
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'genbank'):
+            self.assertEqual(True, rec.id in ['NC_004347.2', 'NC_000913.3'])
+            count += 1
+
+        f.close()
+        self.assertEqual(2, count)
+
+    def test_export_sequence_namespace_feat_prot(self):
+        """Export from namespace as translated proteins."""
+        fixture_params = ['-D', 'deltas', '-o', 'feat-prot']
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + fixture_params)
+
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'fasta'):
+            count += 1
+
+        f.close()
+        self.assertEqual(3430, count)
+
+    def test_export_sequence_namespace_feat_nucl(self):
+        """Export from namespace as untranslated proteins."""
+        fixture_params = ['-D', 'deltas', '-o', 'feat-nucl']
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + fixture_params)
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'fasta'):
+            count += 1
+
+        f.close()
+        self.assertEqual(3485, count)
+
+    def test_export_sequence_namespace_fasta(self):
+        """Export from namespace as fasta."""
+        fixture_params = ['-D', 'deltas', '-o', 'fasta']
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + fixture_params)
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'fasta'):
+            self.assertEqual('NC_002939.5', rec.id)
+            count += 1
+
+        f.close()
+        self.assertEqual(1, count)
+
+    def test_export_sequence_namespace_genbank(self):
+        """Export from taxonomy, qualifier and value as genbank."""
+        fixture_params = ['-D', 'deltas', '-o', 'gb']
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.database_connection_params + self.common_params + fixture_params)
+        self.assertEqual(result.exit_code, 0)
+        f = StringIO(result.output)
+        count = 0
+        for rec in SeqIO.parse(f, 'genbank'):
+            self.assertEqual('NC_002939.5', rec.id)
+            count += 1
+
+        f.close()
+        self.assertEqual(1, count)
