77639e1ae5572e6e4eb4ed2ad56c674d03115f47
ctSkennerton <c.skennerton@gmail.com>
2018-07-11 15:39:18 -0700

    add in more tests for biosqlx add sequence

--- tests/test_biosqlx.py
+++ tests/test_biosqlx.py
@@ -34,6 +34,67 @@ class TestAddSequence(unittest.TestCase):
         """Tear down test fixtures, if any."""
         os.remove(self.dbname)
 
+    def test_add_no_file(self):
+        """Check Error when no file is given"""
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.common_params + ['-D', 'test'])
+        self.assertNotEqual(result.exit_code, 0)
+
+    def test_add_only_fasta(self):
+        """Check Error when only a fasta file is given"""
+        infile = os.path.join(os.path.dirname(__file__), 'test_files', 'GCF_000005845.2_ASM584v2_genomic.fna')
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.common_params + ['-f', infile, '-D', 'test'])
+        self.assertNotEqual(result.exit_code, 0)
+
+    def test_add_only_gff(self):
+        """Check Error when only a fasta file is given"""
+        infile = os.path.join(os.path.dirname(__file__), 'test_files', 'GCF_000005845.2_ASM584v2_genomic.gff')
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.common_params + ['-g', infile, '-D', 'test'])
+        self.assertNotEqual(result.exit_code, 0)
+
+    def test_add_from_gff(self):
+        """Add in sequences from a gff + fasta file."""
+        gff = os.path.join(os.path.dirname(__file__), 'test_files', 'GCF_000005845.2_ASM584v2_genomic.gff')
+        fasta = os.path.join(os.path.dirname(__file__), 'test_files', 'GCF_000005845.2_ASM584v2_genomic.fna')
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.common_params + ['-g', gff, '-f', fasta, '-D', 'test'])
+        self.assertEqual(result.exit_code, 0)
+
+        server = BioSeqDatabase.open_database(driver = self.dbdriver, user = self.dbuser,
+                             passwd = self.dbpassword, host = self.dbhost, db = self.dbname)
+
+        rows = server.adaptor.execute_and_fetchall("SELECT name FROM taxon_name where name_class = 'scientific name'")
+        self.assertEqual(rows, [])
+        server.close()
+
+    def test_add_from_gff_with_taxonomy(self):
+        """Add in sequences from a gff + fasta file given taxonomy."""
+        gff = os.path.join(os.path.dirname(__file__), 'test_files', 'GCF_000005845.2_ASM584v2_genomic.gff')
+        fasta = os.path.join(os.path.dirname(__file__), 'test_files', 'GCF_000005845.2_ASM584v2_genomic.fna')
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.common_params + ['-t', '-T', 511145, '-g', gff, '-f', fasta, '-D', 'test'])
+        self.assertEqual(result.exit_code, 0)
+
+        server = BioSeqDatabase.open_database(driver = self.dbdriver, user = self.dbuser,
+                             passwd = self.dbpassword, host = self.dbhost, db = self.dbname)
+
+        rows = server.adaptor.execute_and_fetchall("SELECT name FROM taxon_name where name_class = 'scientific name'")
+        dbnames = set([x[0] for x in rows])
+        names = set(['cellular organisms',
+                    'Bacteria',
+                    'Proteobacteria',
+                    'Gammaproteobacteria',
+                    'Enterobacterales',
+                    'Enterobacteriaceae',
+                    'Escherichia',
+                    'Escherichia coli',
+                    'Escherichia coli K-12',
+                    'Escherichia coli str. K-12 substr. MG1655'])
+        self.assertCountEqual(dbnames, names)
+        server.close()
+
     def test_add_from_genbank(self):
         """Add in sequences from a Genbank file."""
         infile = os.path.join(os.path.dirname(__file__), 'test_files', 'GCF_000005845.2_ASM584v2_genomic.gbff')
@@ -48,8 +109,100 @@ class TestAddSequence(unittest.TestCase):
         self.assertEqual(rows, [('Escherichia coli str. K-12 substr. MG1655',)])
         server.close()
 
+    def test_add_from_genbank_tax_lookup(self):
+        """Add in sequences from a Genbank file and lookup taxonomy."""
+        infile = os.path.join(os.path.dirname(__file__), 'test_files', 'GCF_000005845.2_ASM584v2_genomic.gbff')
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.common_params + ['-t', '-G', infile, '-D', 'test'])
+        self.assertEqual(result.exit_code, 0)
+
+        server = BioSeqDatabase.open_database(driver = self.dbdriver, user = self.dbuser,
+                             passwd = self.dbpassword, host = self.dbhost, db = self.dbname)
+
+        rows = server.adaptor.execute_and_fetchall("SELECT name FROM taxon_name where name_class = 'scientific name'")
+        dbnames = set([x[0] for x in rows])
+        names = set(['cellular organisms',
+                    'Bacteria',
+                    'Proteobacteria',
+                    'Gammaproteobacteria',
+                    'Enterobacterales',
+                    'Enterobacteriaceae',
+                    'Escherichia',
+                    'Escherichia coli',
+                    'Escherichia coli K-12',
+                    'Escherichia coli str. K-12 substr. MG1655'])
+        self.assertCountEqual(dbnames, names)
+        server.close()
+
+    def test_add_from_genbank_tax_override(self):
+        """Add in sequences from a Genbank file and override the taxonomy in the file."""
+        infile = os.path.join(os.path.dirname(__file__), 'test_files', 'GCF_000005845.2_ASM584v2_genomic.gbff')
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.common_params + ['-T', 656, '-t', '-G', infile, '-D', 'test'])
+        self.assertEqual(result.exit_code, 0)
+
+        server = BioSeqDatabase.open_database(driver = self.dbdriver, user = self.dbuser,
+                             passwd = self.dbpassword, host = self.dbhost, db = self.dbname)
+
+        rows = server.adaptor.execute_and_fetchall("SELECT name FROM taxon_name where name_class = 'scientific name'")
+        dbnames = set([x[0] for x in rows])
+        names = set(['cellular organisms',
+                    'Bacteria',
+                    'Proteobacteria',
+                    'Gammaproteobacteria',
+                    'Aeromonadales',
+                    'Aeromonadaceae',
+                    'Aeromonas',
+                    'Aeromonas allosaccharophila'])
+        self.assertCountEqual(dbnames, names)
+        server.close()
+
+    def test_add_from_genbank_novel_tax(self):
+        """Add in sequences from a Genbank file and provide novel taxons."""
+        infile = os.path.join(os.path.dirname(__file__), 'test_files', 'GCF_000005845.2_ASM584v2_genomic.gbff')
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.common_params + ['-T', 1236, '-t', '-G', infile, '-D', 'test', 'NewSpecies:species'])
+        self.assertEqual(result.exit_code, 0)
+
+        server = BioSeqDatabase.open_database(driver = self.dbdriver, user = self.dbuser,
+                             passwd = self.dbpassword, host = self.dbhost, db = self.dbname)
+
+        rows = server.adaptor.execute_and_fetchall("SELECT name FROM taxon_name where name_class = 'scientific name'")
+        dbnames = set([x[0] for x in rows])
+        names = set(['cellular organisms',
+                    'Bacteria',
+                    'Proteobacteria',
+                    'Gammaproteobacteria',
+                    'NewSpecies'])
+        self.assertCountEqual(dbnames, names)
+        server.close()
+
+    def test_add_from_genbank_novel_tax2(self):
+        """Add in sequences from a Genbank file and provide multiple novel taxons."""
+        infile = os.path.join(os.path.dirname(__file__), 'test_files', 'GCF_000005845.2_ASM584v2_genomic.gbff')
+        runner = CliRunner()
+        result = runner.invoke(cli.main, self.common_params + ['-T', 1236, '-t', '-G', infile, '-D',
+            'test', 'NovelFamily:family', 'NovelGenus:genus', 'NewSpecies:species'])
+        self.assertEqual(result.exit_code, 0)
+
+        server = BioSeqDatabase.open_database(driver = self.dbdriver, user = self.dbuser,
+                             passwd = self.dbpassword, host = self.dbhost, db = self.dbname)
+
+        rows = server.adaptor.execute_and_fetchall("SELECT name FROM taxon_name where name_class = 'scientific name'")
+        dbnames = set([x[0] for x in rows])
+        names = set(['cellular organisms',
+                    'Bacteria',
+                    'Proteobacteria',
+                    'Gammaproteobacteria',
+                    'NovelFamily',
+                    'NovelGenus',
+                    'NewSpecies'])
+        self.assertCountEqual(dbnames, names)
+        server.close()
+
+
 class TestExportSequence(unittest.TestCase):
-    """Tests for `biosqlx` package."""
+    """Tests for `biosqlx export sequence`."""
 
     def setUp(self):
         """Set up test fixtures, if any."""
